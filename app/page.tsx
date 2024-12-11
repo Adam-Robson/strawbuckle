f767bc51ebfcf3d221a861dc27f8ef4e2ee6bd18
Adam Robson <adamrayrobson@gmail.com>
2024-12-10 21:50:36 -0800

    add navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import AudioPlayer from "./_components/audio-player/AudioPlayer";
 import Background from "./_components/Background";
+import Navigation from "./_components/Navigation";
 import { AudioProvider } from "./_contexts/AudioProvider";
 import "./page.css";
 
 export default function Home() {
   return (
     <div className="homepage-container">
+      <header>
+        <Navigation />
+      </header>
       <main>
         <Background src="/fishhed.webp" alt="le fog" height={900} width={900} />
       </main>
